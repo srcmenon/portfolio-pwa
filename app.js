d5cfd26caf5d7457e6133f5c5b1b0d16a821efca
srcmenon <srcmenon002@gmail.com>
2026-03-06 16:50:52 +0100

    Update app.js
    removed old unwanted buttons

--- app.js
+++ app.js
@@ -186,9 +186,6 @@ return allocation;
 }
 window.addEventListener("load", () => {
 
-let addBtn = document.getElementById("addAsset");
-
-if(addBtn){
 let saveBtn = document.getElementById("saveAsset");
 
 if(saveBtn){
@@ -222,6 +219,7 @@ buyDate:buyDate
 });
 
 loadAssets();
+
 document.getElementById("assetName").value="";
 document.getElementById("assetTicker").value="";
 document.getElementById("assetQty").value="";
@@ -229,40 +227,10 @@ document.getElementById("assetPrice").value="";
 };
 }
 
-let name = prompt("Asset name");
-if(!name) return;
-
-let ticker = prompt("Ticker (example: NVDA)");
-let broker = prompt("Broker (Kite / Scalable / TradeRepublic / eToro)");
-let type = prompt("Asset type (Stock / ETF / Crypto / Mutual Fund)");
-let qty = parseFloat(prompt("Quantity")) || 0;
-let price = parseFloat(prompt("Buy price")) || 0;
-let currency = prompt("Currency (EUR / USD / INR)");
-let buyDate = prompt("Buy date (YYYY-MM-DD)");
-
-let tx = db.transaction("assets","readwrite");
-
-tx.objectStore("assets").add({
-name:name,
-ticker:ticker,
-broker:broker,
-type:type,
-quantity:qty,
-buyPrice:price,
-currentPrice:price,
-currency:currency,
-buyDate:buyDate
-});
-
-loadAssets();
-
-};
-}
-
 loadAssets();
 
 if(navigator.onLine){
-setInterval(() => {
+setInterval(()=>{
 if(navigator.onLine){
 updatePrices();
 }
