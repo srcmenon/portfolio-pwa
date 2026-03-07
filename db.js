f7c64fd36dd20e2ed2e2eab88b3af281b50351f9
srcmenon <srcmenon002@gmail.com>
2026-03-07 22:13:24 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,31 +2,15 @@ let db;
 
 function initDB(){
 
-let request = indexedDB.open("portfolioDB",3);
+let request = indexedDB.open("portfolioDB",1);
 
 request.onupgradeneeded = (event) => {
 
 db = event.target.result;
 
-/* ASSETS */
-
 if(!db.objectStoreNames.contains("assets")){
 db.createObjectStore("assets",{keyPath:"id",autoIncrement:true});
 }
-
-/* TRANSACTIONS */
-
-if(!db.objectStoreNames.contains("transactions")){
-db.createObjectStore("transactions",{keyPath:"id",autoIncrement:true});
-}
-
-/* DIVIDENDS */
-
-if(!db.objectStoreNames.contains("dividends")){
-db.createObjectStore("dividends",{keyPath:"id",autoIncrement:true});
-}
-
-/* PORTFOLIO HISTORY */
 
 if(!db.objectStoreNames.contains("portfolioHistory")){
 db.createObjectStore("portfolioHistory",{keyPath:"timestamp"});
@@ -34,6 +18,16 @@ db.createObjectStore("portfolioHistory",{keyPath:"timestamp"});
 
 };
 
-request.onsuccess = e => db = e.target.result;
+request.onsuccess = (event) => {
+
+db = event.target.result;
+
+/* start app only after DB is ready */
+
+loadAssets();
+updatePrices();
+recordPortfolioSnapshot();
+
+};
 
 }
