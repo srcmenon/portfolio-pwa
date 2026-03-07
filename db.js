ff705356beebb48438f4cb8a0982473bb65a84af
srcmenon <srcmenon002@gmail.com>
2026-03-07 19:50:33 +0100

    Update db.js
    for portfolio chart

--- db.js
+++ db.js
@@ -11,9 +11,23 @@ db = e.target.result;
 db.createObjectStore("assets",{keyPath:"id",autoIncrement:true});
 db.createObjectStore("transactions",{keyPath:"id",autoIncrement:true});
 db.createObjectStore("dividends",{keyPath:"id",autoIncrement:true});
+db.createObjectStore("portfolioHistory", {
+keyPath: "timestamp"
+});
+};
+request.onupgradeneeded = (event) => {
+
+db = event.target.result;
+
+if(!db.objectStoreNames.contains("assets")){
+db.createObjectStore("assets",{keyPath:"id",autoIncrement:true});
+}
+
+if(!db.objectStoreNames.contains("portfolioHistory")){
+db.createObjectStore("portfolioHistory",{keyPath:"timestamp"});
+}
 
 };
-
 request.onsuccess = e => db = e.target.result;
 
 }
