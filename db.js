863c873831b182fb6ae61a146ca3d735255b86fc
srcmenon <srcmenon002@gmail.com>
2026-03-07 19:58:27 +0100

    Update db.js
    db upgrade

--- db.js
+++ db.js
@@ -4,30 +4,36 @@ function initDB(){
 
 let request = indexedDB.open("portfolioDB",1);
 
-request.onupgradeneeded = e => {
-
-db = e.target.result;
-
-db.createObjectStore("assets",{keyPath:"id",autoIncrement:true});
-db.createObjectStore("transactions",{keyPath:"id",autoIncrement:true});
-db.createObjectStore("dividends",{keyPath:"id",autoIncrement:true});
-db.createObjectStore("portfolioHistory", {
-keyPath: "timestamp"
-});
-};
 request.onupgradeneeded = (event) => {
 
 db = event.target.result;
 
+/* ASSETS */
+
 if(!db.objectStoreNames.contains("assets")){
 db.createObjectStore("assets",{keyPath:"id",autoIncrement:true});
 }
+
+/* TRANSACTIONS */
+
+if(!db.objectStoreNames.contains("transactions")){
+db.createObjectStore("transactions",{keyPath:"id",autoIncrement:true});
+}
+
+/* DIVIDENDS */
+
+if(!db.objectStoreNames.contains("dividends")){
+db.createObjectStore("dividends",{keyPath:"id",autoIncrement:true});
+}
+
+/* PORTFOLIO HISTORY */
 
 if(!db.objectStoreNames.contains("portfolioHistory")){
 db.createObjectStore("portfolioHistory",{keyPath:"timestamp"});
 }
 
 };
+
 request.onsuccess = e => db = e.target.result;
 
 }
