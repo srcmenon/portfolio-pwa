0e1d8a45abf665a021b2b3cd0d1d42cbca31b389
srcmenon <srcmenon002@gmail.com>
2026-03-14 22:40:06 +0100

    Update db.js

--- db.js
+++ db.js
@@ -57,38 +57,4 @@ function initDB(){
   request.onerror = event => {
     console.error("IndexedDB failed to open:", request.error);
   };
-}let db;
-
-function initDB(){
-
-let request = indexedDB.open("portfolioDB",1);
-
-request.onupgradeneeded = (event) => {
-
-db = event.target.result;
-
-if(!db.objectStoreNames.contains("assets")){
-db.createObjectStore("assets",{keyPath:"id",autoIncrement:true});
-}
-
-if(!db.objectStoreNames.contains("portfolioHistory")){
-db.createObjectStore("portfolioHistory",{keyPath:"timestamp"});
-}
-
-};
-
-request.onsuccess = (event) => {
-
-db = event.target.result;
-
-if(typeof startApp === "function"){
-startApp();
-}
-
-};
-
-request.onerror = (event) => {
-console.error("IndexedDB failed to open:", request.error);
-};
-
 }
